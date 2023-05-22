a61927bc846db61d0fd2910a80685d634bca887f
LipatJob <lipatjobj@gmail.com>
2023-05-22 22:11:10 +0800

    added clear button for search

--- components/SearchFilter.js
+++ components/SearchFilter.js
@@ -6,11 +6,13 @@ export default function SearchFilter({ onSubmit }) {
     <div className="flex flex-row gap-3 items-center">
       <label className="text-xl">Search</label>
       <input
-        type="text"
+        type="search"
         className="search sort rounded py-2 px-4 text-lg"
         value={searchText}
         onChange={(e) => setSearchText(e.target.value)}
+        placeholder="Enter pokemon name or ID"
       />
+
       <button
         className="submit-search bg-red-500 py-2 px-4 rounded text-white"
         type="button"
